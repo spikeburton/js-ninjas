bcdb980becbafc7ac0946053305dd104ae80242e
spikeburton <ralphburton3@gmail.com>
2019-06-20 10:09:31 -0400

    added methods for queue functionality using functional programming

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,18 +1,36 @@
-var Queue = function(){
+var Queue = function() {
   var someInstance = {};
 
   // Use an object with numeric keys to store values
   var storage = {};
+  /**
+   * {
+   *   1: 'spike',
+   *   2: 'duncan',
+   *   3: 'cody'
+   * }
+   */
+  var theHead = 0;
+  var theTail = 0;
 
   // Implement the methods below
 
-  someInstance.enqueue = function(value){
+  someInstance.enqueue = function(value) {
+    storage[theTail] = value;
+    theTail++;
   };
 
-  someInstance.dequeue = function(){
+  someInstance.dequeue = function() {
+    if (theTail - theHead === 0) return;
+
+    var shifted = storage[theHead];
+    delete storage[theHead];
+    theHead++;
+    return shifted;
   };
 
-  someInstance.size = function(){
+  someInstance.size = function() {
+    return theTail - theHead;
   };
 
   return someInstance;
